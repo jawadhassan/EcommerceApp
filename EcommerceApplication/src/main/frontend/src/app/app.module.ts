bbf96915fb558df8331aec88dc6af471cb4ee91f
Jawad Hassan Soomro <jawadsom@yahoo.com>
2019-05-02 16:58:17 +0500

    appcomponent and other files included

--- EcommerceApplication/src/main/frontend/src/app/app.module.ts
+++ EcommerceApplication/src/main/frontend/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { EcommerceComponent } from './ecommerce/ecommerce.component';
 import { ProductsComponent } from './ecommerce/products/products.component';
 import { OrdersComponent } from './ecommerce/orders/orders.component';
 import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
+import { EcommerceService } from './ecommerce/service/ecommerce-service.service';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,12 @@ import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.c
     ShoppingCartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [EcommerceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
